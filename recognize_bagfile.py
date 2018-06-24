566a83f653bae24bfe4777012e6ed5e5011b57d3
Guru Subramani <guru.subramani.g.s@gmail.com>
2018-06-24 12:10:46 -0500

    combined two models

--- recognize_bagfile.py
+++ recognize_bagfile.py
@@ -35,44 +35,79 @@ if __name__ == "__main__":
 
     bfr = bagfile_reader(bagfile)
 
-
-    wrench1_,wrencht1 = bfr.get_topic_msgs("/ftmini40")
-    wrench2_,wrencht2 = bfr.get_topic_msgs("/ftmini402")
+    wrench1_, wrencht1 = bfr.get_topic_msgs("/ftmini40")
+    wrench2_, wrencht2 = bfr.get_topic_msgs("/ftmini402")
     # labels_,labelst = bfr.get_topic_msgs("/labels")
 
     timesamples = wrencht1
 
     # labels = [label_messsage.data for label_messsage in labels_]
 
-    wrench1 = np.array([[f.wrench.force.x,f.wrench.force.y,f.wrench.force.z,
-                    f.wrench.torque.x,f.wrench.torque.y,f.wrench.torque.z] for f in wrench1_])
+    wrench1 = np.array([[f.wrench.force.x, f.wrench.force.y, f.wrench.force.z,
+                         f.wrench.torque.x, f.wrench.torque.y, f.wrench.torque.z] for f in wrench1_])
 
-    wrench2 = np.array([[f.wrench.force.x,f.wrench.force.y,f.wrench.force.z,
-                    f.wrench.torque.x,f.wrench.torque.y,f.wrench.torque.z] for f in wrench2_])
+    wrench2 = np.array([[f.wrench.force.x, f.wrench.force.y, f.wrench.force.z,
+                         f.wrench.torque.x, f.wrench.torque.y, f.wrench.torque.z] for f in wrench2_])
 
+    wrench1 = np.array([np.interp(timesamples, wrencht1, wrench1[:, ii]) for ii in range(6)]).transpose()
+    wrench2 = np.array([np.interp(timesamples, wrencht2, wrench2[:, ii]) for ii in range(6)]).transpose()
 
-    wrench1 = np.array([np.interp(timesamples,  wrencht1, wrench1[:,ii]) for ii in range(6)]).transpose()
-    wrench2 = np.array([np.interp(timesamples,  wrencht2, wrench2[:,ii]) for ii in range(6)]).transpose()
+    data = np.transpose(np.append(wrench1, wrench2, axis=1)).tolist()
 
-
-    data = np.transpose(np.append(wrench1,wrench2,axis = 1)).tolist()
-
-    #addings stuff to the database
-    sb = SignalBundle(data,timesamples)
+    # addings stuff to the database
+    sb = SignalBundle(data, timesamples)
     ld = LabeledData(sb)
     data = [ld]
 
     timestamps = np.array(ld.signal_bundle.timestamps)
 
+    ####################################################################################################################
+    ##  Wavelet Recognition  ###########################################################################################
+    ####################################################################################################################
 
     # estimation
-    features = (0,5)
-    X_signal,_ = get_signal_features(data)
-    X_wavelet, _ = get_wavelet_features(data,features=features)
-    X = np.append(X_signal,X_wavelet,axis = 1)
+    # training data
+    features = (0, 5)
+    X_signal, _ = get_signal_features(data)
+    X_wavelet, _ = get_wavelet_features(data, features=features)
+    X = np.append(X_signal, X_wavelet, axis=1)
+
+    # Loading and configuring model parameters
+    window = 100
+    batch_stride = 1
+    batch_size = 1
+    num_classes = len(label_names_df)
+    data_dim = np.shape(X)[1]
 
     model_LSTM_saved = load_model("./trained_models/LSTM_wavelet_100ep_5feat.hdf5")
+    model_LSTM = LSTM_model(data_dim, window, batch_size, num_classes, stateful=False)
+    model_LSTM.set_weights(model_LSTM_saved.get_weights())
 
+    # Creating data blocks for training the model
+    skip = 100
+    sg = SeqGen(X[0:-1:skip], timestamps[0:-1:skip], 1, window, batch_stride=1)
+
+    predictions = np.array([]).reshape(0, len(label_names_df))
+    predictions_timestamps = np.array([])
+    for ii in range(len(sg)):
+        predictions = np.append(predictions, model_LSTM.predict(sg[ii][0], batch_size=batch_size), axis=0)
+        predictions_timestamps = np.append(predictions_timestamps, sg[ii][1], axis=0)
+
+    predictions_wavelets = sortify(predictions, predictions_timestamps.tolist())
+
+    predict_df = pd.DataFrame(predictions, columns=label_names_df)
+    predict_list = list(predict_df.idxmax(axis=1))
+
+    predict_list_sorted = sortify(predict_list, predictions_timestamps.tolist())
+
+    predict_list_sorted_wavelet = predict_list_sorted
+
+    ####################################################################################################################
+    ##  LSTM_only Recognition  #########################################################################################
+    ####################################################################################################################
+
+    X_signal, Y = get_signal_features(data)
+    X = X_signal
 
     window = 100
     batch_stride = 1
@@ -80,50 +115,48 @@ if __name__ == "__main__":
     num_classes = len(label_names_df)
     data_dim = np.shape(X)[1]
 
+    model_LSTM_saved = load_model("./trained_models/LSTM_only.hdf5")
     model_LSTM = LSTM_model(data_dim, window, batch_size, num_classes, stateful=False)
-
     model_LSTM.set_weights(model_LSTM_saved.get_weights())
 
     skip = 100
 
+    sg = SeqGen(X[0:-1:skip], timestamps[0:-1:skip], 1, window, batch_stride=1)
 
-    # batch_size = np.shape(X[0:-1:skip])[0]
-    # replacing timestamps for Y because we need the corresponding timestamps for each sample in the batch
-    # sg = SeqGen(X,Y ,batch_size,window,batch_stride=batch_stride) # #replacing Y with timestamps
-    sg = SeqGen(X[0:-1:skip],timestamps[0:-1:skip],1,window,batch_stride=1)
-
-    predictions = np.array([]).reshape(0,len(label_names_df))
+    predictions = np.array([]).reshape(0, len(label_names_df))
     predictions_timestamps = np.array([])
+
     for ii in range(len(sg)):
-        predictions = np.append(predictions,model_LSTM.predict(sg[ii][0], batch_size=batch_size),axis = 0)
-        predictions_timestamps = np.append(predictions_timestamps,sg[ii][1],axis = 0)
+        predictions = np.append(predictions, model_LSTM.predict(sg[ii][0], batch_size=batch_size), axis=0)
+        predictions_timestamps = np.append(predictions_timestamps, sg[ii][1], axis=0)
+
+    predictions_LSTM_only = sortify(predictions, predictions_timestamps.tolist())
 
     predict_df = pd.DataFrame(predictions, columns=label_names_df)
     predict_list = list(predict_df.idxmax(axis=1))
 
-    predict_list_sorted = sortify(predict_list,predictions_timestamps.tolist())
-
-    print predict_list_sorted
-
+    predict_list_sorted = sortify(predict_list, predictions_timestamps.tolist())
+    predict_list_sorted_LSTM_only = predict_list_sorted
 
     plt.figure(1)
-    ax = plt.subplot(1, 1, 1)
-
-    plotResult_colorbars(predict_list_sorted, range(len(predict_list_sorted)),
+    ax = plt.subplot(3, 1, 1)
+    plotResult_colorbars(predict_list_sorted_wavelet, range(len(predict_list_sorted_wavelet)),
                          labelNames=list(label_names_df) + [''], ax=ax, medfiltwidth=1)
+    ax.set_xlim([0, len(predict_list_sorted_wavelet)])
+
+    ax = plt.subplot(3, 1, 2)
+    plotResult_colorbars(predict_list_sorted_LSTM_only, range(len(predict_list_sorted_LSTM_only)),
+                         labelNames=list(label_names_df) + [''], ax=ax, medfiltwidth=1)
+    ax.set_xlim([0, len(predict_list_sorted_wavelet)])
+
+    predictions_combined = np.array(predictions_wavelets) * np.array(predictions_LSTM_only)
+
+    predict_df = pd.DataFrame(predictions_combined, columns=label_names_df)
+    predict_list = list(predict_df.idxmax(axis=1))
+
+    ax = plt.subplot(3, 1, 3)
+    plotResult_colorbars(predict_list, range(len(predict_list)),
+                         labelNames=list(label_names_df) + [''], ax=ax, medfiltwidth=11)
+    ax.set_xlim([0, len(predict_list_sorted_wavelet)])
 
     plt.show()
-    exit()
-
-
-
-    labels_t = sg[0][1]
-
-    string_message_list = []
-
-    for label in predict_list:
-        message = String()
-        message.data = label
-        string_message_list.append(message)
-
-    write_to_bagfile(bagfile, '/labels',string_message_list, labels_t, 'a', createbackup=True)
--- recognize_bagfile_wavelet.py
+++ recognize_bagfile_wavelet.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 from plot_generator import plotResult_colorbars
 import pandas as pd
 from sklearn.utils.class_weight import compute_sample_weight
-from time_series_learning import get_signal_features,LSTM_model,SeqGen,sortify
+from time_series_learning import get_signal_features,LSTM_model,SeqGen,sortify,get_wavelet_features
+
 from keras.callbacks import TensorBoard
 from keras.models import load_model
 import cPickle as pickle
@@ -31,7 +32,7 @@ if __name__ == "__main__":
     bagfile = args.bagfile
 
     label_names_df = pickle.load(open("label_names_df.pkl", "rb"))
-    print label_names_df
+
     bfr = bagfile_reader(bagfile)
 
 
@@ -63,23 +64,14 @@ if __name__ == "__main__":
 
     timestamps = np.array(ld.signal_bundle.timestamps)
 
+
     # estimation
+    features = (0,5)
+    X_signal,_ = get_signal_features(data)
+    X_wavelet, _ = get_wavelet_features(data,features=features)
+    X = np.append(X_signal,X_wavelet,axis = 1)
 
-    X_signal,Y = get_signal_features(data)
-    # X_wavelet, _ = get_wavelet_features(data)
-    # X = np.append(X_signal,X_wavelet,axis = 1)
-
-    # Y_one_hot = pd.get_dummies(Y,columns=label_names_df)
-    Y_one_hot = np.zeros((len(X_signal),len(label_names_df)))
-    # print np.shape(Y_one_hot)
-    # print Y_one_hot,label_names_df
-    # exit()
-
-    # X = np.append(X_signal,np.roll(np.zeros(np.shape(Y_one_hot)),1,axis=0),axis = 1)
-    # X = np.append(X_signal,Y_one_hot,axis = 1)
-    X = X_signal
-
-    model_LSTM_saved = load_model("./trained_models/LSTM_only.hdf5")
+    model_LSTM_saved = load_model("./trained_models/LSTM_wavelet_100ep_5feat.hdf5")
 
 
     window = 100
@@ -87,7 +79,6 @@ if __name__ == "__main__":
     batch_size = 1
     num_classes = len(label_names_df)
     data_dim = np.shape(X)[1]
-    print np.shape(X)
 
     model_LSTM = LSTM_model(data_dim, window, batch_size, num_classes, stateful=False)
 
@@ -95,10 +86,11 @@ if __name__ == "__main__":
 
     skip = 100
 
-    Y_one_hot = pd.get_dummies(Y,columns=label_names_df)
-    # sg = SeqGen(X[0:-1:skip],Y_one_hot[0:-1:skip],1,window,batch_stride=1)
-    sg = SeqGen(X[0:-1:skip], timestamps[0:-1:skip], 1, window, batch_stride=1)
 
+    # batch_size = np.shape(X[0:-1:skip])[0]
+    # replacing timestamps for Y because we need the corresponding timestamps for each sample in the batch
+    # sg = SeqGen(X,Y ,batch_size,window,batch_stride=batch_stride) # #replacing Y with timestamps
+    sg = SeqGen(X[0:-1:skip],timestamps[0:-1:skip],1,window,batch_stride=1)
 
     predictions = np.array([]).reshape(0,len(label_names_df))
     predictions_timestamps = np.array([])
@@ -109,58 +101,21 @@ if __name__ == "__main__":
     predict_df = pd.DataFrame(predictions, columns=label_names_df)
     predict_list = list(predict_df.idxmax(axis=1))
 
-    print predict_list
-
-
-    predict_list_sorted = sortify(predict_list,predictions_timestamps)
+    predict_list_sorted = sortify(predict_list,predictions_timestamps.tolist())
 
     print predict_list_sorted
 
-    ###################################################################################################################
-    ###################################################################################################################
-    ###################################################################################################################
-
-    Y_one_hot = pd.get_dummies(np.roll(predict_list_sorted,1),columns=label_names_df)
-    sg2 = SeqGen(Y_one_hot, predictions_timestamps, 1, window, batch_stride=1)
-
-    model_LSTM_saved = load_model("./trained_models/LSTM_only_augmented.hdf5")
-
-    window = 100
-    batch_stride = 1
-    batch_size = 1
-    num_classes = len(label_names_df)
-    data_dim = np.shape(Y_one_hot)[1]
-
-
-    model_LSTM = LSTM_model(data_dim, window, batch_size, num_classes, stateful=False)
-
-    model_LSTM.set_weights(model_LSTM_saved.get_weights())
-
-    predictions_filtered = np.array([]).reshape(0,len(label_names_df))
-    predictions_timestamps_filtered = np.array([])
-    for ii in range(len(sg)):
-        predictions_filtered = np.append(predictions,model_LSTM.predict(sg2[ii][0], batch_size=batch_size),axis = 0)
-        predictions_timestamps_filtered = np.append(predictions_timestamps_filtered,sg[ii][1],axis = 0)
-
-    predict_df = pd.DataFrame(predictions_filtered, columns=label_names_df)
-    predictions_filtered_list = list(predict_df.idxmax(axis=1))
-
-    predict_list_sorted_filtered = sortify(predictions_filtered_list,predictions_timestamps_filtered)
-
 
     plt.figure(1)
-    ax = plt.subplot(2, 1, 1)
+    ax = plt.subplot(1, 1, 1)
 
     plotResult_colorbars(predict_list_sorted, range(len(predict_list_sorted)),
                          labelNames=list(label_names_df) + [''], ax=ax, medfiltwidth=1)
 
-    ax = plt.subplot(2, 1, 2)
-
-    plotResult_colorbars(predict_list_sorted_filtered, range(len(predict_list_sorted_filtered)),
-                         labelNames=list(label_names_df) + [''], ax=ax, medfiltwidth=1)
-
     plt.show()
     exit()
+
+
 
     labels_t = sg[0][1]
 
