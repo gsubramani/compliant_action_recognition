3ef3f06926ec8609ede667a1a40a899cd7105050
Guru Subramani <guru.subramani.g.s@gmail.com>
2018-06-24 10:41:49 -0500

    added statefull training - this takes a long time

--- LSTM_wavelet_training_statefull.py
+++ LSTM_wavelet_training_statefull.py
@@ -3,20 +3,18 @@ from plot_generator import plotResult_colorbars
 import pandas as pd
 import numpy as np
 from sklearn.utils.class_weight import compute_sample_weight
-from time_series_learning import label_map,relabel_data,get_signal_features,LSTM_model,SeqGen,load_db
+from time_series_learning import label_map,relabel_data,get_signal_features,LSTM_model,SeqGen,load_db,get_wavelet_features
 from keras.callbacks import TensorBoard
 from keras.models import load_model
-import argparse
 import cPickle as pickle
 
 
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('epochs')
 
-    epochs = 100
+    epochs = 10
     continue_training = False
+
 
     data = load_db('grab_objects', path='./')
     data = relabel_data(data, label_map)
@@ -24,48 +22,46 @@ if __name__ == "__main__":
     test_data = data[10:-2]
     validation_data = data[-2:]
 
+    features = (0,5)
 
-    trainX,trainY = get_signal_features(training_data)
+    trainX_signal,trainY = get_signal_features(training_data)
+    trainX_wavelet, _ = get_wavelet_features(training_data,features=features)
+    trainX = np.append(trainX_signal,trainX_wavelet,axis = 1)
+
     train_sample_weights = compute_sample_weight('balanced', trainY)
     Y_train_one_hot = pd.get_dummies(trainY)
 
+    testX_signal ,testY = get_signal_features(test_data)
+    testX_wavelet, _ = get_wavelet_features(test_data,features=features)
+    testX = np.append(testX_signal,testX_wavelet,axis = 1)
 
-    trainX = np.roll(np.array(Y_train_one_hot),1,axis=0)
 
-    # print np.roll(np.array(Y_train_one_hot),1,axis = 1)
-    # exit()
-
-    testX ,testY = get_signal_features(test_data)
     test_sample_weights = compute_sample_weight('balanced', testY)
     Y_test_one_hot = pd.get_dummies(testY)
 
-    testX = np.roll(np.array(Y_test_one_hot),1,axis=0)
+    validationX_signal, validationY = get_signal_features(validation_data)
+    validationX_wavelet, _ = get_wavelet_features(validation_data,features=features)
+    validationtX = np.append(validationX_signal,validationX_wavelet,axis = 1)
 
-
-    validationX, validationY = get_signal_features(validation_data)
     validation_sample_weights = compute_sample_weight('balanced', validationY)
     Y_validation_one_hot = pd.get_dummies(validationY)
 
-    validationX =  np.roll(np.array(Y_validation_one_hot),1,axis=0)
+
+
 
     label_names_df = list(Y_train_one_hot.columns.values)
 
     pickle.dump(label_names_df, open("label_names_df.pkl", "wb"))
 
+
     data_dim = np.shape(trainX)[1]
     timesteps = 100
-    batch_size = 100
+    batch_size = 1
     num_classes = len(label_names_df)
 
-    model_LSTM = LSTM_model(data_dim, timesteps, batch_size, num_classes, stateful=False)
+    model_LSTM = LSTM_model(data_dim, timesteps, batch_size, num_classes, stateful=True)
 
-    # if continue_training:
-    #     try:
-    #         model_LSTM  = load_model("./trained_models/LSTM_only.hdf5")
-    #         print "loading model"
-    #     except:
-    #         "model doesn't exist!"
-    #         exit()
+
 
     skip = 100
     trainX_sampled = trainX[0:-1:skip]
@@ -84,8 +80,7 @@ if __name__ == "__main__":
     data_dim = np.shape(trainX)[1]
     timesteps = 100
     window = 100
-    batch_stride = 100
-    batch_size = 100
+    batch_stride = 1
 
     sg_train = SeqGen(trainX_sampled,trainY_one_hot_sampled,batch_size,window,
                       batch_stride=batch_stride,sample_weights=sw_train_sampled)
@@ -96,15 +91,15 @@ if __name__ == "__main__":
     sg_validation = SeqGen(validationX_sampled,validationY_one_hot_sampled,batch_size,window,
                       batch_stride=batch_stride, sample_weights=sw_validation_sampled)
 
-
-    tb = TensorBoard(log_dir='./LSTM_only_augmented', histogram_freq=0,
+    tb = TensorBoard(log_dir='./LSTM_wavelets', histogram_freq=0,
                                 write_graph=True, write_images=True)
+
+
 
     model_LSTM.fit_generator(sg_train,validation_data=sg_validation,
                         epochs=epochs,callbacks=[tb])
 
-
-    model_LSTM.save("./trained_models/LSTM_only_augmented.hdf5")
+    model_LSTM.save("./trained_models/LSTM_wavelet_10ep_5feat_statefull.hdf5")
 
     test_one_hot_predict_series = []
     test_one_hot_truth_series = []
@@ -137,4 +132,3 @@ if __name__ == "__main__":
 
 
     plt.show()
-
